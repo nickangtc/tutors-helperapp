0e026fbee0eb12e094b1b6fbe1b68b25fa191788
Nick Ang <nickangtc@gmail.com>
2016-11-01 11:24:07 +0800

    Customised Devise user controller to add default admin=false

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  # Use Devise's default controllers
+  # but use customised Registrations controller
+  devise_for :users, controllers: { registrations: "users/registrations" }
+
 
   resources :lessons
+
   resources :posts do
     resources :comments
   end
 
-  get '/users/:id', to: 'users#show'
+  # Landing page
   root 'main#index'
+
+  # Frontpage if logged in
+  get '/users/:id', to: 'users#show'
+
 end
