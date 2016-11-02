d23c531ad846fd34dc2b78bc1a3a073518447f6d
Nick Ang <nickangtc@gmail.com>
2016-11-02 14:54:11 +0800

    Generated new payments method model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102064624) do
+ActiveRecord::Schema.define(version: 20161102065127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20161102064624) do
     t.boolean  "student_has_read_note"
     t.string   "label",                 default: "lesson"
     t.integer  "paid_method"
+  end
+
+  create_table "payment_methods", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
