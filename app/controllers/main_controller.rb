badbe3db8f4044686e653d7c7eca0c67e23bcdec
Nick Ang <nickangtc@gmail.com>
2016-11-03 22:58:10 +0800

    Added classes overview to student homepage

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,12 @@
 class MainController < ApplicationController
   def index
     @user = current_user
-    if @user!= nil && !@user.admin
-      redirect_to '/users/' + current_user.id.to_s
-    else
-      render 'index'
-    end
+    @lessons_past = @user.lessons
+                        .where.not( start_time: 10.years.ago..Time.now )
+                        .order("start_time DESC")
+
+    @lessons_upcoming = @user.lessons
+                            .where.not( start_time: 10.years.ago..Time.now )
+                            .order("start_time DESC")
   end
 end
