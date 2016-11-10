410df077758845a9832f05c6b33baeacb2094a5f
Nick Ang <nickangtc@gmail.com>
2016-11-10 12:48:47 +0800

    Changed content type to text in db. Forum posts now preserve line spacing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110004953) do
+ActiveRecord::Schema.define(version: 20161110044252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 20161110004953) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
